9284dd3170e4512cdb12df85a8bde45eede067d0
Dmitriy Gerasimenko <gerasimenko@corepartners.ru>
2021-06-29 00:08:11 +0300

    initial commit

---
+++ librarymanager/src/test/java/com/github/deerge/librarymanager/LibraryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.deerge.librarymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
