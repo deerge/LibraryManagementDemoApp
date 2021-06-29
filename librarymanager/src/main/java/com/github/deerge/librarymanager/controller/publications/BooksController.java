a7fb983ebcdcbcc90c21f7226a1d5a0f8c3e50a4
Dmitriy Gerasimenko <gerasimenko@corepartners.ru>
2021-06-30 00:53:15 +0300

    #1 fix controllers paths

--- librarymanager/src/main/java/com/github/deerge/librarymanager/controller/publications/BooksController.java
+++ librarymanager/src/main/java/com/github/deerge/librarymanager/controller/publications/BooksController.java
@@ -8,7 +8,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController()
-@RequestMapping("/book")
+@RequestMapping("/books")
 public class BooksController {
     private final BooksApi booksApi;
 
--- librarymanager/src/main/java/com/github/deerge/librarymanager/controller/publications/JournalsController.java
+++ librarymanager/src/main/java/com/github/deerge/librarymanager/controller/publications/JournalsController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController()
-@RequestMapping("/journal")
+@RequestMapping("/journals")
 public class JournalsController {
     private final JournalApi journalApi;
 
