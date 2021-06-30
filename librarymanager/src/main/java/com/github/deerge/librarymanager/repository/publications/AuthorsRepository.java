a84acd3851bc69881347a52bd9c51007524a51e6
Dmitriy Gerasimenko <gerasimenko@corepartners.ru>
2021-06-30 12:03:03 +0300

    #1 fix repository request

--- librarymanager/src/main/java/com/github/deerge/librarymanager/repository/publications/AuthorsRepository.java
+++ librarymanager/src/main/java/com/github/deerge/librarymanager/repository/publications/AuthorsRepository.java
@@ -1,9 +1,7 @@
 package com.github.deerge.librarymanager.repository.publications;
 
 import com.github.deerge.librarymanager.model.publications.Author;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,9 +9,7 @@ import java.util.Optional;
 @Repository
 public interface AuthorsRepository extends CrudRepository<Author, Long> {
 
-    @Query("from Author a where a.firstName = :firstName and a.lastName = :lastName " +
-            "and a.middleName = :middleName")
-    Optional<Author> findAuthorByFirstNameAndLastNameAndMiddleName(@Param("firstName") String firstName,
-                                                                   @Param("lastName") String lastName,
-                                                                   @Param("middleName") String middleName);
+    Optional<Author> findAuthorByFirstNameAndLastNameAndMiddleName(String firstName,
+                                                                   String lastName,
+                                                                   String middleName);
 }
